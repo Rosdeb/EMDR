14e637e67e62720c356d79f004cc8ea6cc23be17
Nirob Hasan <mursalinnirob@gmail.com>
2026-02-11 05:15:19 +0600

    upadate date 10

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color mainAppColor = Color(0xFF4A7C59);
   static const Color border = Color(0xFFE3E6F0);
+  static const Color bgColor = Color(0xFF60655C);
+  // static const Color textDark = Color(0xFF4A4A4A);
 
 }
