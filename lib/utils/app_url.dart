94d490918bc128f07ae0b1739c57b54e1f4651d7
Nirob Hasan <mursalinnirob@gmail.com>
2026-05-06 18:06:55 +0600

    progress , simulation

--- lib/utils/app_url.dart
+++ lib/utils/app_url.dart
@@ -1,4 +1,4 @@
 class AppEnv {
   // Update this single URL whenever your Cloudflare Tunnel changes.
-    static const String baseUrl = 'https://incorporated-laura-alan-arranged.trycloudflare.com/api';
+    static const String baseUrl = 'https://joins-karaoke-suppliers-stop.trycloudflare.com/api';
 }
